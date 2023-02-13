5a711e1cf303401a5f2ef52ec2bbb7d6014afa41
FerryDeVries <71122367+FerryDeVries@users.noreply.github.com>
2023-02-13 09:56:56 +0100

    Index toegevoegd

---
+++ js/data.js
@@ -0,0 +1,37 @@
+var jsonData = [
+    {
+        "q" : "Een programmeertaal is?",
+        "opt1" : "Een taal die alle Spanjaarden begrijpen",
+        "opt2" : "Een taal die alle Nederlanders begrijpen",
+        "opt3" : "Een taal die computers begrijpen",
+        "answer" : "Een taal die computers begrijpen"
+    },
+    {
+        "q" : "Een statement is?",
+        "opt1" : "Één regel van een algoritme",
+        "opt2" : "Een setje regels van een algoritme",
+        "opt3" : "Een verzameling algoritmes",
+        "answer" : "Één regel van een algoritme"
+    },
+    {
+        "q" : "Een algoritme is?",
+        "opt1" : "Een verzameling instructies zonder volgorde",
+        "opt2" : "Een reeks opeenvolgende instructies",
+        "opt3" : "Een programma uitvoeren",
+        "answer" : "Een reeks opeenvolgende instructies"
+    },
+    {
+        "q" : "Als ik aan het runnen ben",
+        "opt1" : "Laat ik een computer een programma opstarten",
+        "opt2" : "Laat ik een computer een algoritme doorlopen",
+        "opt3" : "Laat ik een computer afsluiten",
+        "answer" : "Laat ik een computer een algoritme doorlopen"
+    },
+    {
+        "q" : "Debuggen is?",
+        "opt1" : "Fouten uit een algoritme halen",
+        "opt2" : "Ongedierte en virussen uit de computer halen",
+        "opt3" : "Een computer een algoritme foutloos laten uitvoeren",
+        "answer" : "Fouten uit een algoritme halen"
+    }
+];
